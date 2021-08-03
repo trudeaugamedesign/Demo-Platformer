d3ba079acd8d9a694212b05b0836dd83f2eb7a87
Ducky <masterless123@gmail.com>
2021-08-03 14:17:27 -0400

    Add player jump movement

---
+++ Demo Platformer/Assets/Scripts/EnableJumping.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableJumping : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        PlayerMovement player = other.gameObject.GetComponent<PlayerMovement>();
+
+        // If this is not true, then the collider was not the player, rather
+        // some other object.
+        if (player)
+        {
+            player.canJump = true;
+        }
+    }
+}
--- Demo Platformer/Assets/Scripts/PlayerMovement.cs
+++ Demo Platformer/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
+    public float jumpSpeed;
+
+    [HideInInspector] public bool canJump = false;
     private Rigidbody2D playerRb;
 
     void Start()
@@ -14,8 +17,22 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        // For player movement
         float inputX = Input.GetAxis("Horizontal");
-
         playerRb.velocity = new Vector2 (inputX * movementSpeed, playerRb.velocity.y);
+
+        // Check if able to jump
+        float inputJump = Input.GetAxis("Jump");
+        if (canJump && inputJump > 0.0f)
+        {
+            StartCoroutine("Jump");
+        }
+    }
+
+    IEnumerator Jump()
+    {
+        playerRb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        canJump = false;
+        yield return null;
     }
 }
